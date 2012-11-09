5c58e39e55a083e7f3d72507f78144315bee000c
Felipe Amorim <felipe.leusin@gmail.com>
2012-11-09 16:29:27 -0200

    created structure

---
+++ src/MyBlog.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CloakedRobot.Web.MvcApplication" Language="C#" %>
---
+++ src/MyBlog.Web/Models/Post.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyBlog.Web.Models
+{
+    public class Post
+    {
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Introduction { get; set; }
+
+        public string CanonicalUrl { get; set; }
+
+        public string Slug { get; set; }
+
+        public DateTimeOffset DateCreated { get; set; }
+
+        public DateTimeOffset DatePublished { get; set; }
+
+        public int ViewCount { get; set; }
+            
+    }
+}
